2635bdc4895c0422a0fbcd943748574a33248a7d
jwrunner3224 <joybrother2019@gmail.com>
2020-02-15 21:10:05 +0200

    complete

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import data from './tables.json'
+
+function getKeys() {
+  return Object.keys(data['rows'][0]);
+}
+
+function getRowsData() {
+  var keys = getKeys();
+  return data.rows.map((row: any, index: any) => {
+    return <tr key={index}>{
+      keys.map((key, sub_index) => {
+        return <td key={sub_index}><div dangerouslySetInnerHTML={{ __html: row[key] }}></div></td>
+      })}
+    </tr>
+  })
+}
+function App() {
+  return (
+    <div className="bodyClass">
+      <table>
+        <tbody>
+          {getRowsData()}
+        </tbody>
+      </table>
+    </div>);
+}
+
+export default App;
